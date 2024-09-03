20fb852dfecc6c9de756a2a25114c9c8ff599234
jMurad <jml.murad@gmail.com>
2024-09-03 07:02:02 +0300

    Delete reshenie.py

--- reshenie.py
+++
@@ -1,9 +0,0 @@
-def durak():
-    spisok = {'brat1': [1, 'Murad', 234], 'brat2': 'maga', 'brat3': 'Shamil'}
-    for s in spisok:
-        if type(spisok[s]) == list:
-            print(spisok[s][1])
-        else:
-            print(spisok[s])
-
-durak()
